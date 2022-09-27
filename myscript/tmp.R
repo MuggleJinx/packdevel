48de753e5b84cc434517bf8d29b3f2d3839e5584
Xiaotian Jin <jinxiaotian_sal@outlook.com>
2022-09-27 18:08:59 +0300

    init

--- myscript/tmp.R
+++ myscript/tmp.R
@@ -5,3 +5,15 @@ load_all()
 Rcpp::compileAttributes()
 
 timesThree(3)
+check()
+
+.Library
+.libPaths()
+?INSTALL
+?install.packages()
+
+getOption("repos")
+install.packages("ngme2")
+
+check()
+wait_a_second_omp(4,4)
